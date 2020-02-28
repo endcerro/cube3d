7a108c8f5258c96f3166eefc3745cf4e0ef784e0
Enzo DAL  CERRO <edal--ce@e2r2p2.42.fr>
2020-02-28 05:42:55 +0100

    hey that's pretty good

--- src/wc_utils.c
+++ src/wc_utils.c
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 20:24:45 by edal--ce          #+#    #+#             */
-/*   Updated: 2020/02/28 05:26:45 by edal--ce         ###   ########.fr       */
+/*   Updated: 2020/02/28 05:40:43 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,15 @@ int			hit_wall(t_contr *contr, t_col_rend *r, t_vpi *map)
 
 int			get_tx_id(int side, t_vpi step)
 {
+	//0 1 2 3
 	if (side == 1 && step.y > 0)
-		return (0);
+		return (1); //R1 S
 	else if (side == 1 && step.y < 0)
-		return (1);
+		return (0); //R0 N
 	else if (side == 0 && step.x > 0)
-		return (2);
-	else
 		return (3);
+	else
+		return (2);
 }
 
 void		init_r(t_contr *contr, t_col_rend *r)
